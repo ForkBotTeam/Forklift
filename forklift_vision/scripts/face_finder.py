93924523ffe0226db32b0fc087a631f2b9ad17ce
Abdullah Hussien <abdullahhussien.ah@gmail.com>
2024-03-28 01:42:06 +0200

    Added many functionalities aiding towards the funal target in this sequence: - "get_depth" were modified to return the depth map in the "DEPTH_REGISTERED 11-BIT format" which contains depth info in mm and the pixel locations i & j are in propertion with the real x & y coordinates - "how_far" were modified to simply index the depth value in mm from the now accurate depth map - "isolate" is a function that aims to remove objects further or closer than a certain margin from the frame aiding in the process of acurate edge finding. Returns a mask with the desired profile - "detect_edge" applies some filters then detects edges in an image using "HoughLinep" - "real_coordinates" transforms pixel values i & j to real-world coordinates x & y - "face_finder" was aiming to find the plane equation between two lines but approach requires rethinking

--- forklift_vision/scripts/face_finder.py
+++ forklift_vision/scripts/face_finder.py
@@ -4,6 +4,7 @@ import freenect
 import cv2
 import numpy as np
 import rospy
+from itertools import combinations
 
 class corner_finder():
     def __init__(self):
@@ -17,8 +18,10 @@ class corner_finder():
         self.frame_original = np.empty([480,640,3])
 
         # Fake YOLO, create a slice aimed at the box
-        posx = slice(130,300)
-        posy = slice(280,480)
+        posx = slice(170,400)
+        posy = slice(214,390)
+        self.rangex = posx.stop - posx.start
+        self.rangey = posy.stop - posy.start
         SCx = int((posx.stop+posx.start)/2) #slice_centerx
         SCy = int((posy.stop+posy.start)/2) #slice_centery
         refx = slice((SCx-10),(SCx+10))
@@ -28,11 +31,15 @@ class corner_finder():
         decreasing_array = np.arange((posy.stop-posy.start+6)/2, 3, -1)
         both_halves = np.hstack([decreasing_array, decreasing_array[::-1]])
         self.valley = np.tile(both_halves, (posx.stop-posx.start, 1))
-       
+
+        self.box_lines = None
+        self.edges = 0
+        tictoc = rospy.get_time()
         while 1:
             self.frame = self.get_video() #get a frame from RGB camera
             self.frame_original = np.copy(self.frame)
             self.quantitative_depth, self.visual_depth = self.get_depth() 
+            
             #>>> Visual depth: normalized for displaying. 
             #>>> Quantitative depth: has correct distacne values in mm 
 
@@ -43,15 +50,20 @@ class corner_finder():
             gray = cv2.cvtColor(self.frame_original[posx, posy],cv2.COLOR_BGR2GRAY)
             self.display(gray*mask, "isolated", 3)
 
-            self.corner(self.frame_original[posx,posy], mask, posx.start, posy.start) 
+            self.detect_edge(self.frame_original[posx,posy], mask, posx.start, posy.start) 
+            real_lines = None
+            # try:
+            if self.box_lines is not None:
+                x1, y1, z1 = self.real_coordinates(self.box_lines[:,0:1], self.box_lines[:,1:2], 640, 480)
+                x2, y2, z2 = self.real_coordinates(self.box_lines[:,2:3], self.box_lines[:,3:4], 640, 480)
 
-            i ,j = 336,190
-            # self.real_coordinates(i, j, (posx.stop-posx.start), (posy.stop-posy.start))
-            self.real_coordinates(i, j, 640, 480)
+                real_lines = np.hstack([x1,y1,z1,x2,y2,z2])
+
+            # except:
+            #     pass
+            self.face_finder(real_lines)
+       
             
-            cv2.circle(self.frame, (i, j), 2, (200,100,20),2)
-
-
 
             self.display(self.frame_original[posx,posy], "original", 0)
             self.display(self.frame, "cornered", 1)
@@ -61,57 +73,40 @@ class corner_finder():
             if k == 27:
                 # cv2.imwrite('Documents/fork/src/forklift_vision/images/depth_pic.jpg', self.visual_depth)
                 break
+            # print(rospy.get_time()-tictoc)
+            tictoc = rospy.get_time()
             rate.sleep()
 
         freenect.sync_stop()
         cv2.destroyAllWindows()
 
-    def how_far(self, x, y, **kwargs): # "depth" are kinect depth frame, x,y are pixel locations
-        # Remember x-axis translates to indexing of coloumns while y-axis is indexing of rows!
-        dist = str(self.quantitative_depth[y,x]/1000)
-        if "print" in kwargs:
-            cv2.putText(kwargs["print"], dist,(x+5, y+5 ),fontFace= 3, fontScale= 0.75, color= (150,20,20),thickness= 2)    
-        return dist
+    def face_finder(self,lines):
+        if lines is not None:
+            x1 = lines[:,0:1]
+            y1 = lines[:,1:2]
+            z1 = lines[:,2:3]
+            
+            x2 = lines[:,3:4]   
+            y2 = lines[:,4:5]
+            z2 = lines[:,5:6]
 
-    def isolate(self, depth, ref):
-        margin = 500
-        # depth = depth.transpose()
-        # img = np.array([(depth < ref+margin) & (depth > ref-margin)]*3).transpose() * img
+            line_vectors = np.hstack([x2-x1 , y2-y1 , z2-z1])
+            no_of_lines = line_vectors.shape[0]
 
-        # img = ((depth < ref+margin) & (depth > ref-margin)) 
-        # mask = np.ones(img.shape, dtype=np.uint8)
-        # mask = img*mask
-        pos_ref = ref + self.valley*8
-        neg_ref = ref - self.valley*8
-        condition = ((depth < pos_ref) & (depth > neg_ref))
-        mask = np.ones(depth.shape, dtype=np.uint8)*condition
-        
-        return mask
+            # We want to find all possible planes between these vectors
+            if no_of_lines > 1:
+                mixat = np.array(list(combinations(line_vectors,2)))
+                planes = np.cross(mixat[:,0],mixat[:,1])
 
-    def corner(self, target, mask, xbound, ybound):
-        #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-        # EDGE DETECTION USING HOUGH-TRANSFORM METHOD  
-        mask = cv2.medianBlur(mask, 15)
-        gray = cv2.cvtColor(target, cv2.COLOR_BGR2GRAY) 
-        gray = cv2.medianBlur(gray, 5)
+                # print(f"no. of planes: {planes.shape[0]}\n")
+                plane_norm = np.linalg.norm(planes, axis=1)
+                # print(plane_norm)
+                plane_angles = np.arccos((planes[:,2])/plane_norm)
+                # print(plane_angles*180/np.pi)  
 
-        gray *= mask
-        gray = np.uint8(gray)
-
-        self.display(gray,"blurred", 4)
-
-        edges = cv2.Canny(gray, 100, 150, apertureSize=3)
-        self.display(edges, "canny",5)        
-        lines = cv2.HoughLinesP(edges, 1, np.pi/180,50, None, 100,20)
-        
-        try:
-            # The below for loop runs till r and theta values are in the range of the 2d array
-            for i in range(0, len(lines)):
-                l = lines[i][0]
-                cv2.line(self.frame, (l[0]+ybound, l[1]+xbound), (l[2]+ybound, l[3]+xbound), (0, 0, 255), 2, cv2.LINE_AA)
-        except :
-            # print("There are no lines BRO!")
-            pass
+            else:
+                pass
+                # print("there are no planes")
 
     def real_coordinates(self,i, j, w, h):
         # Coordinate from camera's prespective
@@ -128,11 +123,72 @@ class corner_finder():
         z = self.quantitative_depth[j, i]
 
         if isinstance(i, np.ndarray):
-            
+            for a in range(0, i.shape[0]):
+                self.how_far(i[a,0], j[a,0])
 
-        x = (i - w / 2) * (z + minDistance) * scaleFactor
-        y = (j - h / 2) * (z + minDistance) * scaleFactor
-        print(f"x: {x:.2f} y: {y:.2f} z: {z:.2f}",end="\r")
+            x = (i - w / 2) * (z + minDistance) * scaleFactor
+            y = (j - h / 2) * (z + minDistance) * scaleFactor
+        else:
+            x = (i - w / 2) * (z + minDistance) * scaleFactor
+            y = (j - h / 2) * (z + minDistance) * scaleFactor
+            self.how_far(i, j, print = self.frame)
+
+        return x,y,z
+    
+    def how_far(self, x, y, **kwargs): # "depth" are kinect depth frame, x,y are pixel locations
+        # Remember x-axis translates to indexing of coloumns while y-axis is indexing of rows!
+        dist = str(self.quantitative_depth[y,x]/1000)
+
+        if "print" in kwargs:
+            cv2.putText(kwargs["print"], dist,  (int(x+5),int(y+5)),fontFace= 3, fontScale= 0.75, color= (150,20,20),thickness= 2)    
+        return dist
+
+    def detect_edge(self, target, mask, xbound, ybound):
+        #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+        # EDGE DETECTION USING HOUGH-TRANSFORM METHOD  
+        gray = cv2.cvtColor(target, cv2.COLOR_BGR2GRAY) 
+        gray = cv2.medianBlur(gray, 5)
+
+        gray *= mask
+        gray = np.uint8(gray)
+
+        self.display(gray,"blurred", 4)
+
+        edges = cv2.Canny(gray, 50, 150, apertureSize=3)
+
+        self.display(edges, "canny",5)  
+
+        voting = int(np.min([self.rangex, self.rangey])/2)-20  # no of voting points = min edge of the bounding box - a margin      
+        lines = cv2.HoughLinesP(edges, 5, 5*np.pi/180, 10, None, 10, 50)
+        cv2.imwrite('Documents/fork/src/forklift_vision/images/depth_pic.jpg', self.visual_depth)
+
+        
+        try:
+            # The below for loop runs till r and theta values are in the range of the 2d array
+            self.box_lines = lines[:,0] + np.array([ybound, xbound, ybound, xbound])
+            for i in range(0, len(lines)):
+                # l = lines[i][0]
+                cv2.line(self.frame, (self.box_lines[i,0], self.box_lines[i,1]), (self.box_lines[i,2], self.box_lines[i,3]), (0, 0, 255), 2, cv2.LINE_AA)
+        except :
+            # print("There are no lines BRO!")
+            pass
+
+    def isolate(self, depth, ref):
+        # margin = 500
+        # depth = depth.transpose()
+        # img = np.array([(depth < ref+margin) & (depth > ref-margin)]*3).transpose() * img
+
+        # img = ((depth < ref+margin) & (depth > ref-margin)) 
+        # mask = np.ones(img.shape, dtype=np.uint8)
+        # mask = img*mask
+        pos_ref = ref + self.valley*8
+        neg_ref = ref - self.valley*8
+        condition = ((depth < pos_ref) & (depth > neg_ref))
+        mask = np.ones(depth.shape, dtype=np.uint8)*condition
+        smoother = int(np.max([self.rangex, self.rangey])/45)*2 + 3
+        mask = cv2.medianBlur(mask, smoother)
+        
+        return mask
 
     def get_video(self): #function to get RGB image from kinect
         d = freenect.sync_get_video()
@@ -149,6 +205,7 @@ class corner_finder():
 
         if d is not None:
             depth, _ = d
+            cv2.medianBlur(depth, 5, dst=depth)
             visualize_array = cv2.normalize(depth,dst=None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
             visualize_array = visualize_array.astype(np.uint8)
         else:
